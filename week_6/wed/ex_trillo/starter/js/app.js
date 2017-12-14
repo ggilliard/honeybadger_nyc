7f3b8cd2cccb73ade9afc4aa6bd8f9f37ba7b649
LadyG <ghgilliard@gmail.com>
2017-12-13 21:28:50 -0500

    save

---
+++ week_6/wed/ex_trillo/starter/js/app.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    // document.getElementById("todo-column").addEventListener("click", onClick);
+    // function onClick(event) {
+    //     alert("Hey, I told you not to hover over me!");
+    // }
+
+    document.getElementById("todo-column").addEventListener("click", function(event) {
+      // display the current click count inside the clicked div
+      event.target.textContent = "click count: " + event.detail;
+    // }, false);
+
+// const removeButton = function ()
+
+//     const newCard = document.getElementById("new-card");
+
+//     const userInput = ""
+//    const newItem = document.getElementById("newItem");
+//    const newLi = document.createElement ("li");
+//    newLi.classList.add ("newItem");
+//    newLi.innerHTML = "San Andres";
+//    newItem.appendChild(newLi);
+
+
+
+
+
+}
+
+
