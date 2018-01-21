c704558fb9ab68ed8d9d86f141513d3fa7324da3
LadyG <ghgilliard@gmail.com>
2018-01-21 11:08:35 -0500

    more packages

--- week_11/sundaypractice/server.js
+++ week_11/sundaypractice/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 
+app.use(logger('dev'));
 
 app.get('/', function (req, res) {
     res.send('Hello World');
