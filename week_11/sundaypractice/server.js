7ebc67774fcb9246386cc0b6af85bab723cb55a0
LadyG <ghgilliard@gmail.com>
2018-01-21 11:59:10 -0500

    changes from today

--- week_11/sundaypractice/server.js
+++ week_11/sundaypractice/server.js
@@ -5,7 +5,17 @@ const logger = require('morgan');
 app.use(logger('dev'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Hello Serene');
+});
+
+app.get('/sayHello', function (req, res) {
+    const name = req.query.name
+    res.send(`Hello ${name}`);
+});
+
+app.get('/teamName/:name', function (req, res) {
+    const teamName = req.params.name;
+    res.send(`Hello ${teamName}`);
 });
    
-app.listen(3000)
+app.listen(3200)
